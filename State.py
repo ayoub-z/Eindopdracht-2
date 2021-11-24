06f8a0dffa26c7b22a792b010fb291e6d63507cd
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-11-24 20:47:10 +0100

    Added both a money as well as an inventory system

--- State.py
+++ State.py
@@ -8,7 +8,11 @@ class State:
 		self.current_state = None
 		self.input_response = True
 		self.next_state = None
-		
+		self.deposit = 0
+		self.product = {}
+		self.inventory =  {"Coffee": "10", "Mars": "10", "Chips": "10"}
+		self.product_list = {"1": "Coffee", "2": "Mars", "3": "Chips"}	
+
 	def add_state(self, state, prompt, input_response):
 		# als de state al bestaat, voegen we daar een nieuwe 'promt' en 'input_response' aan toe
 		if state in self.state:
@@ -23,6 +27,27 @@ class State:
 	def get_state(self):
 		return self.state
 
+	def increase_deposit(self, amount):
+		self.deposit += float(amount)
+
+	def decrease_deposit(self, amount):
+		self.deposit -= float(amount)			
+
+	def get_deposit(self):
+		return self.deposit		
+
+	def add_product(self, prod_nm, prod_price):
+		self.product[prod_nm] = prod_price
+
+	def get_product(self):
+		return self.product
+
+	def update_inventory(self, prod_nm):
+		self.inventory[prod_nm] = int(self.inventory[prod_nm]) - 1
+
+	def get_inventory(self):
+		return self.inventory
+
 	def add_transition(self, state, input_response, next_state):
 		# als de transitie vanuit deze state al bestaat, voegen we hier een nieuwe 'input_response' en de 'next_state' aan toe
 		if state in self.transition:
@@ -35,7 +60,7 @@ class State:
 	def get_transition(self):
 		return self.transition
 
-	def acceptor(self, prompt, valids):
+	def acceptor(self, state, prompt, valids):
 		''' Acceptor style finite state machine to prompt for user input'''
 		# als de state geen input vereist (eind state, "refunding" state in dit geval)
 		if not valids:
@@ -46,24 +71,69 @@ class State:
 			# als de state wel input vereist
 			while True:
 				# maak die input lowercase, check of het geldig is en returneer het
-				resp = input(prompt)[0].lower()
-				if resp in valids:
-					return resp
+				if state == "choice":
+					resp = input(prompt).lower()
+					if resp in valids:
+						if resp == "r" or resp == "d":
+							return resp
+						elif self.get_deposit() - float(self.product[resp])  <= 0:
+							print("Not enough deposited. Please pick something cheaper or (d)eposit more..")
+							print(f"Current deposited amount: € {self.get_deposit()}\n")
+						elif int(self.get_inventory()[self.product_list[resp]]) <= 0:
+							print(f"We've run out of {self.product_list[resp]}. We will try to refill it as soon as possible. Maybe pick another option..\n")
+						else:
+								return resp
+				else:
+					resp = input(prompt).lower()
+					if resp in valids:
+						return resp
 					
 	def finite_state_machine(self, initial_state, exit_state):		
 		self.next_state = initial_state
 		self.current_state = self.get_state()[self.next_state]
 
 		while self.input_response != exit_state:
+			current_state_name = (list(self.state.keys())[list(self.state.values()).index(self.current_state)])
+
 			# validate the input
-			self.input_response = self.acceptor(self.current_state['prompt'], self.current_state['input_response'])
+			self.input_response = self.acceptor(current_state_name, self.current_state['prompt'], self.current_state['input_response'])
 			# update what the next state will be
 			self.next_state = self.get_transition()[self.next_state][self.input_response]
 
-			# als een product wordt afgegeven, printen we dat uit
-			if self.next_state == "dispense":
-				products = {"1": "Coffee", "2": "Mars", "3": "Chips"}
-				print(f"You ordered: {products[self.input_response]}.")
+			# update deposted amount when money is deposited
+			if self.next_state == "waiting deposit" and current_state_name != "ready" and current_state_name != "choice":
+				# print("Your input: ", self.input_response)
+				self.increase_deposit(self.input_response)
 
+				print("\nCurrent deposited amount: ", self.get_deposit())
+
+			# als een product wordt afgegeven, printen we dat uit
+			elif self.next_state == "dispense":
+				self.decrease_deposit(self.product[self.input_response])
+				print(f"You ordered: {self.product_list[self.input_response]}.")
+				print(f"\nCurrent deposited amount: € {self.get_deposit()}")
+
+				self.update_inventory(self.product_list[self.input_response])
+				print(f"Remaining amount of {self.product_list[self.input_response]}: {self.get_inventory()[self.product_list[self.input_response]]}")
+			elif self.next_state == "refunding":
+				if self.get_deposit() == 0:
+					print(f"Current deposited amount: € {self.get_deposit()}")
+					print("Nothing to refund..")
+				print(f"\nRefunding: € {self.deposit} ..")
+				self.decrease_deposit(self.deposit)
+				print(f"Current deposited amount: € {self.get_deposit()}")
 			# update what our current state is
 			self.current_state = self.get_state()[self.next_state]
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -8,30 +8,47 @@ if __name__ == "__main__":
 	# each state has a prompt (message) and an input (required input)
 	# first parameter is the state, 2nd is the machine promt and 3rd are the possible inputs
 	state.add_state("ready", "Machine ready: (d)eposit money, or (q)uit? ", "['d','q']")
-	state.add_state("waiting", "Machine waiting: (s)elect a product, or (r)efund? ", "['s','r']")
-	state.add_state("choice", "Which product? (1) Coffee, (2) Mars, (3) Chips, or (r)efund ", "['1', '2', '3', 'r']")
-	state.add_state("dispense", "Machine dispensing: please (r)emove product ", "['r']")
-	state.add_state("refunding", "Refunding money..", "")
+	state.add_state("waiting deposit", "Machine waiting: Only these coins/bills are accepted [0.25, 0.5, 1, 2, 5, 10, 20]. Enter (d) when done, (r) to refund or (q)uit ", "['0.25', '0.5', '1', '2', '5', '10', '20', 'd', 'r', 'q']")
+	state.add_state("waiting selection", "Machine waiting: (s)elect a product, or (r)efund? ", "['s','r']")
+	state.add_state("choice", "Which product? (1) Coffee (€ 0.75), (2) Mars (€ 1.25), (3) Chips (€ 1.50), (r)efund or (d)epost ", "['1', '2', '3', 'r', 'd']")
+	state.add_state("dispense", "Machine dispensing: would you like to (p)urchase more or (r)eceive change? ", "['p', 'r']")
+	state.add_state("refunding", "", "")
 	state.add_state("exit", "", "")	
 
 
 	# adding transitions
 	# each transition links two states with each other, with the input being that link
 	# first parameter is the state, 2nd is the input, and 3rd is the next state after receiving given input
-	state.add_transition("ready", "d", "waiting")	
+	state.add_transition("ready", "d", "waiting deposit")	
 	state.add_transition("ready", "q", "exit")	
 
-	state.add_transition("waiting", "s", "choice")	
-	state.add_transition("waiting", "r", "refunding")	
+	state.add_transition("waiting deposit", "0.25", "waiting deposit")
+	state.add_transition("waiting deposit",  "0.5", "waiting deposit")
+	state.add_transition("waiting deposit",    "1", "waiting deposit")
+	state.add_transition("waiting deposit",    "2", "waiting deposit")
+	state.add_transition("waiting deposit",    "5", "waiting deposit")
+	state.add_transition("waiting deposit",   "10", "waiting deposit")
+	state.add_transition("waiting deposit",   "20", "waiting deposit")
+	state.add_transition("waiting deposit",   "d",  "waiting selection")
+	state.add_transition("waiting deposit",   "r",  "refunding")
+	state.add_transition("waiting deposit",   "q",  "refunding")
+
+	state.add_transition("waiting selection", "s", "choice")	
+	state.add_transition("waiting selection", "r", "refunding")	
 
 	state.add_transition("choice", "1", "dispense")	
 	state.add_transition("choice", "2", "dispense")
 	state.add_transition("choice", "3", "dispense")	
 	state.add_transition("choice", "r", "refunding")	
+	state.add_transition("choice", "d", "waiting deposit")
 
-	state.add_transition("dispense", "r", "ready")	
-
+	state.add_transition("dispense", "r", "refunding")	
+	state.add_transition("dispense", "p", "choice")	
 	state.add_transition("refunding", "", "ready")		
 
+	state.add_product("1", "0.75")
+	state.add_product("2", "1.25")
+	state.add_product("3", "1.5")
 
+	# initialiseer de fsm
 	state.finite_state_machine('ready','q')
